88953c268d0b9066a2301bff992d359cd35233c1
clementpnn <95907390+clementpnn@users.noreply.github.com>
2025-04-16 14:15:05 +0200

    fix: eslint problem

--- src/components/ui/useToast.ts
+++ src/components/ui/useToast.ts
@@ -70,56 +70,56 @@ const addToRemoveQueue = (toastId: string) => {
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "ADD_TOAST": {
-      return {
-        ...state,
-        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT)
+  case "ADD_TOAST": {
+    return {
+      ...state,
+      toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT)
+    }
+  }
+
+  case "UPDATE_TOAST": {
+    return {
+      ...state,
+      toasts: state.toasts.map(t => (t.id === action.toast.id ? { ...t, ...action.toast } : t))
+    }
+  }
+
+  case "DISMISS_TOAST": {
+    const { toastId } = action
+
+    if (toastId) {
+      addToRemoveQueue(toastId)
+    } else {
+      for (const toast of state.toasts) {
+        addToRemoveQueue(toast.id)
       }
     }
 
-    case "UPDATE_TOAST": {
+    return {
+      ...state,
+      toasts: state.toasts.map(t =>
+        t.id === toastId || toastId === undefined
+          ? {
+            ...t,
+            open: false
+          }
+          : t
+      )
+    }
+  }
+
+  case "REMOVE_TOAST": {
+    if (action.toastId === undefined) {
       return {
         ...state,
-        toasts: state.toasts.map(t => (t.id === action.toast.id ? { ...t, ...action.toast } : t))
+        toasts: []
       }
     }
-
-    case "DISMISS_TOAST": {
-      const { toastId } = action
-
-      if (toastId) {
-        addToRemoveQueue(toastId)
-      } else {
-        for (const toast of state.toasts) {
-          addToRemoveQueue(toast.id)
-        }
-      }
-
-      return {
-        ...state,
-        toasts: state.toasts.map(t =>
-          t.id === toastId || toastId === undefined
-            ? {
-                ...t,
-                open: false
-              }
-            : t
-        )
-      }
+    return {
+      ...state,
+      toasts: state.toasts.filter(t => t.id !== action.toastId)
     }
-
-    case "REMOVE_TOAST": {
-      if (action.toastId === undefined) {
-        return {
-          ...state,
-          toasts: []
-        }
-      }
-      return {
-        ...state,
-        toasts: state.toasts.filter(t => t.id !== action.toastId)
-      }
-    }
+  }
   }
 }
 
